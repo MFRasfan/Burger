58b7c4d70e048bc507c5e10699e40a29dd3e8fa7
Rasfan M Faiz <rasfanfaiz@gmail.com>
2019-10-06 22:12:30 -0400

    updated files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,28 @@
-var express = require("express");
-var handlebars = require("express-handlebars");
-var path = require('path');
+var express = require('express');
+var router = express.Router();
+var burger = require("../models/burger.js")
+
+//linking to index.handlebars content
+router.get("/", function (req, res) {
+    burger.all(function (burger_data) {
+        console.log(burger_data);
+        res.render('./index', { burger_data });
+    })
+
+})
+
+
+router.put('/burgers/update', function (req, res) {
+    burger.update(req.body.burger_id, function (result) {
+        res.redirect('/');
+    })
+})
+
+//Post
+router.post("/burgers/create", function (req, res) {
+    burger.create(req.body.burger_name, function (result) {
+        console.log(result);
+        res.redirect('/');
+    })
+})
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,33 @@
+//dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var path = require('path');
+
+var app = express();
+var PORT = process.env.PORT || 4000;
+//css
+// app.use(express.static(path.join(__dirname, "public/static")));
+
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//linking controller route
+var routes = require('./controllers/burgers_controller.js');
+
+app.use(routes);
+
+//Listening to PORT
+app.listen(PORT, function () {
+    console.log("Server listening  on: http://localhost:" + PORT);
+});
